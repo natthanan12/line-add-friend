cb3e9b5cfe8f166b31cbdeb3d416cb2e90b18024
natthanan12 <golf.fear03@gmail.com>
2022-08-24 15:03:30 +0700

    database

--- index.js
+++ index.js
@@ -24,10 +24,21 @@ app.post("/webhook", function(req, res) {
       console.log(req.body.events[0])
       const user = req.body.events[0].source.userId
       const connection = mysql.createConnection({
-        host: "erp-test.cfnxq6b0ia8q.ap-southeast-1.rds.amazonaws.com",
+        host: "bauto-schema.cfnxq6b0ia8q.ap-southeast-1.rds.amazonaws.com",
         username: "admin",
         password: "Technician2020!",
         database: "erp_schema",
+        dialect: "mysql",
+        pool: {
+          max: 20,
+          min: 0,
+          acquire: 600000,
+          idle: 10000
+        },
+        logging: false,
+        dialectOptions: {
+          connectTimeout: 600000
+        }
       })
 
       console.log("insert user")
