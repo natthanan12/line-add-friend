c5144a22c42ec4bd41aa1d7020dae475a23b16e7
natthanan12 <golf.fear03@gmail.com>
2022-08-24 15:34:21 +0700

    database

--- index.js
+++ index.js
@@ -42,48 +42,11 @@ app.post("/webhook", function(req, res) {
       connection.query(query,data, (err, rows, fields) => {
         if (!err) {
           console.log("success")
-          const dataString = JSON.stringify({
-            replyToken: req.body.events[0].replyToken,
-            messages: [
-              {
-                "type": "text",
-                "text": "Welcome To Line Bot BAE"
-              }
-            ]
-          })
-      
-          // Request header
-          const headers = {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer " + TOKEN
-          }
-      
-          // Options to pass into the request
-          const webhookOptions = {
-            "hostname": "api.line.me",
-            "path": "/v2/bot/message/reply",
-            "method": "POST",
-            "headers": headers,
-            "body": dataString
-          }
-      
-          // Define request
-          const request = https.request(webhookOptions, (res) => {
-            res.on("data", (d) => {
-              process.stdout.write(d)
-            })
-          })
-      
-          // Handle error
-          request.on("error", (err) => {
-            console.error(err)
-          })
-      
-          // Send data
-          request.write(dataString)
+          connection.end()
         //   request.end()
         } else {
           console.log(err.message)
+          connection.end()
         }
         connection.end()
       })
