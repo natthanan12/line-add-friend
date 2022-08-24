fe2c8e604eae6fe8f5de4b021186c28fe857c200
natthanan12 <golf.fear03@gmail.com>
2022-08-24 15:30:44 +0700

    database

--- index.js
+++ index.js
@@ -42,6 +42,46 @@ app.post("/webhook", function(req, res) {
       connection.query(query,data, (err, rows, fields) => {
         if (!err) {
           console.log("success")
+          const dataString = JSON.stringify({
+            replyToken: req.body.events[0].replyToken,
+            messages: [
+              {
+                "type": "text",
+                "text": "Welcome To Line Bot BAE"
+              }
+            ]
+          })
+      
+          // Request header
+          const headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + TOKEN
+          }
+      
+          // Options to pass into the request
+          const webhookOptions = {
+            "hostname": "api.line.me",
+            "path": "/v2/bot/message/reply",
+            "method": "POST",
+            "headers": headers,
+            "body": dataString
+          }
+      
+          // Define request
+          const request = https.request(webhookOptions, (res) => {
+            res.on("data", (d) => {
+              process.stdout.write(d)
+            })
+          })
+      
+          // Handle error
+          request.on("error", (err) => {
+            console.error(err)
+          })
+      
+          // Send data
+          request.write(dataString)
+        //   request.end()
         } else {
           console.log(err.message)
         }
@@ -49,45 +89,6 @@ app.post("/webhook", function(req, res) {
       })
 
 
-      const dataString = JSON.stringify({
-        replyToken: req.body.events[0].replyToken,
-        messages: [
-          {
-            "type": "text",
-            "text": "Welcome To Line Bot BAE"
-          }
-        ]
-      })
-  
-      // Request header
-      const headers = {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer " + TOKEN
-      }
-  
-      // Options to pass into the request
-      const webhookOptions = {
-        "hostname": "api.line.me",
-        "path": "/v2/bot/message/reply",
-        "method": "POST",
-        "headers": headers,
-        "body": dataString
-      }
-  
-      // Define request
-      const request = https.request(webhookOptions, (res) => {
-        res.on("data", (d) => {
-          process.stdout.write(d)
-        })
-      })
-  
-      // Handle error
-      request.on("error", (err) => {
-        console.error(err)
-      })
-  
-      // Send data
-      request.write(dataString)
-    //   request.end()
+     
     }
   })
