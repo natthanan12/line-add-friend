c162b63b690ee4ffb7e19275bb4e665587c22cfa
natthanan12 <golf.fear03@gmail.com>
2022-10-14 09:13:47 +0700

    new

--- index.js
+++ index.js
@@ -3,31 +3,35 @@ const express = require("express")
 const mysql = require("mysql")
 const app = express()
 const PORT = process.env.PORT || 3000
-const TOKEN = process.env.LINE_ACCESS_TOKEN || "xxvcCF4XO+EEyIHzn8Tp+RQlv4LGYY6NgsQQX9itLEciOQ7Mzain74h5TMvvzUofgqcUEQMfClwoExI8pbVGqdrV+TrxpKLr8GZoLUh2GXv50hw5oLqNKMrih93Z1jajL1rMKn53Vg7KulvpQDrHNgdB04t89/1O/w1cDnyilFU="
+const TOKEN =
+  process.env.LINE_ACCESS_TOKEN ||
+  "dZ2LaINBI0hiayuo/FpRMi4fsod1dS66LhO4OsW3EdeyiwozxoxeG9pEdjPyQtLco6/rcR3pXQyrfrRReaLS+2JsMmjkTKn3B/qd59634bjIbOu/NPpqM1Js/L/L185+Rgrh+USLMUr3aalUGbyoPAdB04t89/1O/w1cDnyilFU="
 
 app.use(express.json())
-app.use(express.urlencoded({
-  extended: true
-}))
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+)
 
 app.get("/", (req, res) => {
   res.sendStatus(200)
 })
 app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`)
-  })
+  console.log(`Example app listening at http://localhost:${PORT}`)
+})
 
-  
-app.post("/webhook", function(req, res) {
-     res.sendStatus(200)
-    // res.send("HTTP POST request sent to the webhook URL!")
-    // If the user sends a message to your bot, send a reply message
-    console.log(req.body.events[0])
-    if (req.body.events[0].type === "message" || req.body.events[0].type === "follow") {
-      // Message data, must be stringified
-      try{
-
-    
+app.post("/webhook", function (req, res) {
+  res.sendStatus(200)
+  // res.send("HTTP POST request sent to the webhook URL!")
+  // If the user sends a message to your bot, send a reply message
+  console.log(req.body.events[0])
+  if (
+    req.body.events[0].type === "message" ||
+    req.body.events[0].type === "follow"
+  ) {
+    // Message data, must be stringified
+    try {
       console.log(req.body.events[0])
       const user = req.body.events[0].source.userId
       const connection = mysql.createConnection({
@@ -39,25 +43,25 @@ app.post("/webhook", function(req, res) {
         ssl: true,
       })
 
-      console.log("insert user",user)
+      console.log("insert user", user)
       const data = {
-        userName:"",
+        userName: "",
         userId: user,
-        department:""
+        department: "",
       }
       const query = "INSERT INTO lineusers SET ?"
-      connection.query(query,data, (err, rows, fields) => {
+      connection.query(query, data, (err, rows, fields) => {
         if (!err) {
           console.log("success")
           connection.end()
         } else {
           console.log(err)
           connection.end()
-          process.exit(1);
+          process.exit(1)
         }
       })
-    }catch(e){
-        process.exit(1)
+    } catch (e) {
+      process.exit(1)
     }
-    }
-  })
+  }
+})
